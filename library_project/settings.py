b26bd50094fed98559683fd113c60fcc6298cdfe
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-13 01:02:30 +0200

    important comment

--- library_project/settings.py
+++ library_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+# Django-crispy-forms is an application that helps to manage Django forms
 
 # Specify what crispy forms' template pack you will use 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
