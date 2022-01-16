8c71cd281a413428b7d5bd62aae3cf443a5f40e4
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-16 17:27:18 +0200

    more bootstrap

--- library_project/utils.py
+++ library_project/utils.py
@@ -2,17 +2,21 @@ from PIL import Image
 
 
 def is_image_resizable(path):
+    """ Check if image has large resolution, if so, resize it. """
 
     img = Image.open(path)
-    
+
     if img.width > 300 or img.height > 300:
         output_size = (300, 300)
         # that will resize the image
         img.thumbnail(output_size)
         img.save(path)
 
+
 def is_user_admin_or_book_owner(view):
+    """ Check if user is admin or owner of current book. """
+
     book = view.get_object()
     current_user = view.request.user
-        
+
     return current_user == book.posted_by or current_user.is_superuser
