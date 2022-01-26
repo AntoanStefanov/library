c644dbff9203ebbf0398cb15930ebe4cec960688
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-26 17:43:50 +0200

    comment

--- library_project/urls.py
+++ library_project/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
 
 # showing images in browser/development -
 # https://docs.djangoproject.com/en/4.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development
+# https://youtu.be/FdVuKt_iuSI?list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&t=1248
 # add only in dev, check docs for production mode
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
