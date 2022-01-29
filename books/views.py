48230d65217408cc14f9710304f8e9497c57f59d
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-29 17:43:36 +0200

    remove unnecessary imports

--- books/views.py
+++ books/views.py
@@ -1,8 +1,7 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
